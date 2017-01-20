86869cfe76c26f2fd0585bccb4bb1be660a67198
jmjuanes <josemijuanes@gmail.com>
2017-01-20 17:42:30 +0100

    index.js: big rewrite

--- index.js
+++ index.js
@@ -1,156 +1,77 @@
 //Import dependencies
 var fs = require('fs');
 
-//Logty object
-var logty = {};
+//Levels
+var logty_levels = [ 'fatal', 'error', 'warning', 'notice', 'info', 'debug' ];
 
-//Default level
-logty._level = 0;
-
-//Log levels
-logty.levels = [ 'fatal', 'error', 'warning', 'notice', 'info', 'debug' ];
-
-//Parse a level
-logty.parseLevel = function(value)
-{
-  //Check for string
-  if(typeof value === 'string')
-  {
-    //Convert to lower case
-    value = value.toLowerCase();
-
-    //Return the level
-    return (typeof logty.levels.indexOf(value) !== -1) ? value : 'debug';
-  }
-
-  //Check for number
-  if(typeof value === 'number')
-  {
-    //Check the number
-    return (value < 0 || value >= logty.levels.length) ? 'debug' : logty.levels[value];
-  }
-
-  //Return default
-  return 'debug';
-};
-
-//Check the level value
-logty.checkLevel = function(wanted_level, provided_level)
-{
-  //Get the wnted level index
-  var wanted_index = logty.levels.indexOf(wanted_level);
-
-  //Get the provided level index
-  var provided_index = logty.levels.indexOf(provided_level);
-
-  //Check the index level
-  return (wanted_index > provided_index) ? false : true;
-};
-
-//Set the default level
-logty.level = function(value)
-{
-  //Check the value
-  if(typeof value === 'undefined'){ return logty.levels[logty._level]; }
-
-  //Set the level
-  logty._level = logty.parseLevel(value);
-};
-
-//Generate a log message
-logty.message = function(level, message)
-{
-  //Parse the level
-  level = logty.parseLevel(level);
-
-  //Generate the log message
-};
-
-//Display a log in console for each level
-logty.levels.forEach(function(level)
-{
-  //Add the display level
-  logty[level] = function(message)
-  {
-    //Check the level
-    if(logty.checkLevel(logty._level, level) === false){ return; }
-
-    //Generate the message and print on console
-    console.log(logty.message(level, message));
-  };
-
-  //Continue with the next level
-  return true;
-});
-
-//Opened files
-logty._files = [];
+//Files object
+var logty_files = {};
 
 //Cath the process exit event
 process.on('exit', function(code)
 {
-  //Check the files
-  if(logty._files.length === 0){ return; }
-
   //Read all the files
-  for(var key in logty._files)
+  for(var key in logty_files)
   {
     //Close the file stream
-    logty._files[key].end();
+    logty_files[key].end();
   }
 });
 
-//Writer
-logty.writer = function(file, opt)
+//Logty method
+var logty = function(level, file)
 {
-  //Check the file
-  if(typeof file !== 'string'){ throw new Error('Expected string as the log file. Provided ' + typeof file); }
-
-  //Check the options
-  if(typeof opt !== 'object'){ var opt = {}; }
-
-  //Check the encoding option
-  if(typeof opt.encoding !== 'string'){ opt.encoding = 'utf8'; }
-
   //Create the writer id
   this._id = 'writer' + Date.now().toString();
 
-  //Save the print to console option
-  this._console = (typeof opt.console === 'boolean') ? opt.console : true;
+  //Save the level
+  this._level = (typeof level === 'string') ? logty_levels.indexOf(level.toLowerCase()) : level;
 
-  //Get the stream
-  this._stream = fs.createWritableStream(file, { encoding: opt.encoding, flags: 'a' });
+  //Check the level value
+  if(this._level < 0 || logty_levels.length <= this._level){ this._level = logty_levels.indexOf('debug'); }
 
-  //Save the stream object
-  logty._files[this._id] = this._stream;
+  //Get the stream object
+  this._stream = (typeof file === 'string') ? fs.createWriteStream(file, { encoding: 'utf8', flags: 'a' }) : process.stdout;
+
+  //Add this file
+  logty_files[this._id] = this._stream;
 
   //Return this
   return this;
 };
 
-//Build a log message
-logty.writer.prototype.log = function()
+//Close the stream
+logty.prototype.end = function()
 {
-  //
-  //Build the message
-  var log = logty.message()
-};
+  //Check for file stream
+  if(typeof logty_files[this._id] === 'undefined'){ return; }
 
-//Close the writer
-logty.writer.prototype.close = function()
-{
-  //End the stream
+  //Finish the stream
   this._stream.end();
 
-  //Remove the stream
-  delete logty._files[this._id];
+  //Delete from the files list
+  delete logty_files[this._id];
 };
 
-//Reader
-logty.reader = function(file, opt)
+//Display a log message
+logty.prototype.msg = function(level, message)
 {
+  //Check the level
+  if(logty_levels.indexOf(level) < this._level){ return; }
 
+  //Build the message
+  var m = '[ ] [' + level.toUpperCase() + '] ' + message + '\n';
+
+  //Write to the stream
+  this._stream.write(m);
 };
+
+//For each level
+logty_levels.forEach(function(el)
+{
+  //Register the level
+  logty.prototype[el] = function(message){ return this.msg(el, message); };
+});
 
 //Exports to node
 module.exports = logty;
