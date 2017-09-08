520ebbf27bdfdf5b21dab8f7679db457f8475e94
jmjuanes <josemijuanes@gmail.com>
2017-09-08 11:29:02 +0200

    test.js: added file logs test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-//Import logty
+//Import dependencies
 var logty = require('./index.js');
+var fs = require('fs');
 
 //Simple log messages without tag
 var simple_log = new logty('');
@@ -36,3 +37,23 @@ min_log.notice('Tagged notice message');
 min_log.warning('Tagged warning message');
 min_log.error('Tagged error message');
 min_log.fatal('Tagged fatal message');
+
+//Write to a file
+var file_log = new logty(null, fs.createWriteStream('./test.log', { defaultEncoding: 'utf8', flags: 'a' }));
+
+//Register the error event listeners
+file_log.on('error', function(error){ console.error(error); });
+
+//Register the finish event listener
+file_log.on('finish', function(){ console.log('File closed!'); });
+
+//Generate logs
+file_log.debug('Tagged debug message');
+file_log.info('Tagged info message');
+file_log.notice('Tagged notice message');
+file_log.warning('Tagged warning message');
+file_log.error('Tagged error message');
+file_log.fatal('Tagged fatal message');
+
+//Close the stream
+file_log.end();
