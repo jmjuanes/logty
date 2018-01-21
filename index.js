19c0d6ac9f037128326444e73f162574575d1c12
jmjuanes <josemijuanes@gmail.com>
2018-01-21 21:45:43 +0100

    Move some utils to lib folder. Minor changes

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
 var stream = require("stream");
 var util = require("util");
 
-//Default levels list
-let levels = ["fatal", "error", "warning", "notice", "info", "debug"];
-
-//Normalize a number and return it with two digits
-let normalize = function (value) {
-    return ("0" + value).slice(-2);
-};
-
-//Get the default levels
-let getDefaultLevels = function () {
-    return levels.slice(0);
-};
-
-//Get the current day
-let getCurrentDay = function () {
-    let d = new Date();
-    return [d.getFullYear(), normalize(d.getMonth() + 1), normalize(d.getDate())].join("/");
-};
-
-//Get the current time
-let getCurrentTime = function () {
-    let d = new Date();
-    return [normalize(d.getHours()), normalize(d.getMinutes()), normalize(d.getSeconds())].join(":");
-};
-
-//Generate the default message
-let getDefaultMessage = function (tag, day, time, level, message) {
-    let list = [];
-
-    if (tag) {
-        list.push("[" + tag + "]");
-    }
-    list.push("[" + day + " " + time + "]");
-    list.push("[" + level.toUpperCase() + "]");
-    list.push(message.trim());
-
-    return list.join(" ");
-};
+let current = require('./lib/current.js');
+let levels = require('./lib/levels.js');
+let message = require('./lib/message.js');
 
 //Logty readable stream
 var logty = function (opt) {
@@ -56,12 +21,12 @@ var logty = function (opt) {
     stream.Readable.call(this, {encoding: opt.encoding});
 
     this._tag = (typeof opt.tag === "string") ? opt.tag.trim() : null;
-    this._level = levels.length;
+    this._level = levels().length;
     this._disabled = false;
 
     //Default message format
     this._format = function (tag, day, time, level, message) {
-        return getDefaultMessage(tag, day, time, level, message);
+        return message(tag, day, time, level, message);
     };
 
     return this;
@@ -72,11 +37,13 @@ util.inherits(logty, stream.Readable);
 
 //Set the minimum level
 logty.prototype.setLevel = function (level) {
-    if (typeof level === "string" && levels.indexOf(level) > -1) {
-        this._level = levels.indexOf(level);
+    let list = levels();
+    let index = list.indexOf(level);
+    if (typeof level === "string" &&  index > -1) {
+        this._level = index;
     }
     else {
-        this._level = levels.length;
+        this._level = list.length;
     }
     return this;
 };
@@ -91,7 +58,7 @@ logty.prototype.setFormat = function (format) {
 };
 
 //Register a method to emit a log message for each level
-levels.forEach(function (level, index) {
+levels().forEach(function (level, index) {
     logty.prototype[level] = function (text) {
         if (typeof text !== "string") {
             return;
@@ -100,8 +67,8 @@ levels.forEach(function (level, index) {
             return;
         }
 
-        let day = getCurrentDay();
-        let time = getCurrentTime();
+        let day = current.getDay();
+        let time = current.getTime();
         let str = this._format.call(null, this._tag, day, time, level, text);
 
         //Build the message for this level and emit the data event
@@ -125,7 +92,3 @@ logty.prototype.end = function () {
 
 //Exports
 module.exports = logty;
-module.exports.getDefaultMessage = getDefaultMessage;
-module.exports.getCurrentDay = getCurrentDay;
-module.exports.getCurrentTime = getCurrentTime;
-module.exports.getDefaultLevels = getDefaultLevels;
