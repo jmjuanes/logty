46332718679e2fee9a669c1edb87fd70eea3e60b
jmjuanes <josemijuanes@gmail.com>
2018-01-21 19:33:38 +0100

    Reformat variables. Fix setLevel call

--- test.js
+++ test.js
@@ -1,80 +1,79 @@
-//Import dependencies
 var logty = require("./index.js");
 var fs = require("fs");
 
 //Simple log messages without tag
-var simple_log = new logty();
+var simpleLog = new logty();
 
 //Pipe to stdout
-simple_log.pipe(process.stdout);
+simpleLog.pipe(process.stdout);
 
 //Generate simple log messages
-simple_log.debug("This is a debug message");
-simple_log.info("This is an info message");
-simple_log.notice("This is a notice message");
-simple_log.warning("This is a warning message");
-simple_log.error("This is an error message");
-simple_log.fatal("This is a fatal message");
+simpleLog.debug("This is a debug message");
+simpleLog.info("This is an info message");
+simpleLog.notice("This is a notice message");
+simpleLog.warning("This is a warning message");
+simpleLog.error("This is an error message");
+simpleLog.fatal("This is a fatal message");
 
 //Close the log stream
-simple_log.end();
+simpleLog.end();
 
 
 //Log with tag
-var tagged_log = new logty({tag: "my-tag"});
+var taggedLog = new logty({tag: "my-tag"});
 
 //Pipe to stdout
-tagged_log.pipe(process.stdout);
+taggedLog.pipe(process.stdout);
 
 //Generate tagged logs
-tagged_log.debug("Tagged debug message");
-tagged_log.info("Tagged info message");
-tagged_log.notice("Tagged notice message");
-tagged_log.warning("Tagged warning message");
-tagged_log.error("Tagged error message");
-tagged_log.fatal("Tagged fatal message");
+taggedLog.debug("Tagged debug message");
+taggedLog.info("Tagged info message");
+taggedLog.notice("Tagged notice message");
+taggedLog.warning("Tagged warning message");
+taggedLog.error("Tagged error message");
+taggedLog.fatal("Tagged fatal message");
 
 //Close the log stream
-tagged_log.end();
+taggedLog.end();
 
 
 //Log with min level
-var min_log = new logty({tag: "min-log"});
+var minLog = new logty({tag: "min-log"});
 
 //Pipe to stdout
-min_log.pipe(process.stdout);
+minLog.pipe(process.stdout);
 
 //Set the min log level -> hide debug, info and notice levels
-min_log.level("warning");
+minLog.setLevel("warning");
 
 //Generate tagged logs
-min_log.debug("Tagged debug message");
-min_log.info("Tagged info message");
-min_log.notice("Tagged notice message");
-min_log.warning("Tagged warning message");
-min_log.error("Tagged error message");
-min_log.fatal("Tagged fatal message");
+minLog.debug("Tagged debug message");
+minLog.info("Tagged info message");
+minLog.notice("Tagged notice message");
+minLog.warning("Tagged warning message");
+minLog.error("Tagged error message");
+minLog.fatal("Tagged fatal message");
 
 //Close the log stream
-min_log.end();
+minLog.end();
 
 
 //Write to a file
-var file_log = new logty();
+var fileLog = new logty();
 
 //Initialize the writer stream
 var writer = fs.createWriteStream("./test.log", {defaultEncoding: "utf8", flags: "a"});
 
 //Pipe to a writable stream
-file_log.pipe(writer);
+fileLog.pipe(writer);
 
 //Register the error event listeners
-file_log.on("error", function (error) {
+fileLog.on("error", function (error) {
     console.error(error);
 });
 
 //Register the end event listener
-file_log.on("end", function () {
+fileLog.on("end", function () {
     console.log("Log stream closed!");
 });
 
@@ -84,12 +83,12 @@ writer.on("finish", function () {
 });
 
 //Generate logs
-file_log.debug("Tagged debug message");
-file_log.info("Tagged info message");
-file_log.notice("Tagged notice message");
-file_log.warning("Tagged warning message");
-file_log.error("Tagged error message");
-file_log.fatal("Tagged fatal message");
+fileLog.debug("Tagged debug message");
+fileLog.info("Tagged info message");
+fileLog.notice("Tagged notice message");
+fileLog.warning("Tagged warning message");
+fileLog.error("Tagged error message");
+fileLog.fatal("Tagged fatal message");
 
 //Close the stream
-file_log.end();
+fileLog.end();
