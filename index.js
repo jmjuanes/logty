72a3e55f85ad51d4b7c70711a803f78992ad26a8
jmjuanes <josemijuanes@gmail.com>
2018-01-21 19:25:05 +0100

    Minor changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var stream = require('stream');
-var util = require('util');
+var stream = require("stream");
+var util = require("util");
 
 //Default levels list
-var levels = ['fatal', 'error', 'warning', 'notice', 'info', 'debug'];
+var levels = ["fatal", "error", "warning", "notice", "info", "debug"];
 
 //Normalize a number and return it with two digits
 var normalize = function (value) {
-    return ('0' + value).slice(-2);
+    return ("0" + value).slice(-2);
 };
 
 //Default message format function
@@ -14,31 +14,31 @@ var defaultMessage = function(tag, day, time, level, message) {
     var list = [];
 
     if (tag) {
-        list.push('[' + tag + ']');
+        list.push("[" + tag + "]");
     }
-    list.push('[' + day + ' ' + time + ']');
-    list.push('[' + level.toUpperCase() + ']');
+    list.push("[" + day + " " + time + "]");
+    list.push("[" + level.toUpperCase() + "]");
     list.push(message.trim());
 
-    return list.join(' ');
+    return list.join(" ");
 };
 
 //Logty readable stream
 var logty = function (opt) {
-    if (typeof opt !== 'object' || opt === null) {
+    if (typeof opt !== "object" || opt === null) {
         opt = {};
     }
-    if (typeof opt.tag !== 'string') {
+    if (typeof opt.tag !== "string") {
         opt.tag = null;
     }
-    if (typeof opt.encoding !== 'string') {
-        opt.encoding = 'utf8';
+    if (typeof opt.encoding !== "string") {
+        opt.encoding = "utf8";
     }
 
     //Extends the readable stream
     stream.Readable.call(this, {encoding: opt.encoding});
 
-    this._tag = (typeof opt.tag === 'string') ? opt.tag.trim() : null;
+    this._tag = (typeof opt.tag === "string") ? opt.tag.trim() : null;
     this._level = levels.length;
     this._disabled = false;
 
@@ -55,7 +55,7 @@ util.inherits(logty, stream.Readable);
 
 //Set the minimum level
 logty.prototype.setLevel = function (level) {
-    if (typeof level === 'string' && levels.indexOf(level) > -1) {
+    if (typeof level === "string" && levels.indexOf(level) > -1) {
         this._level = levels.indexOf(level);
     }
     else {
@@ -66,8 +66,8 @@ logty.prototype.setLevel = function (level) {
 
 //Set the message format
 logty.prototype.setFormat = function (format) {
-    if (typeof format !== 'function') {
-        throw new Error('Format must be a function');
+    if (typeof format !== "function") {
+        throw new Error("Format must be a function");
     }
     this._format = format;
     return this;
@@ -76,7 +76,7 @@ logty.prototype.setFormat = function (format) {
 //Register a method to emit a log message for each level
 levels.forEach(function (level, index) {
     logty.prototype[level] = function (text) {
-        if (typeof text !== 'string') {
+        if (typeof text !== "string") {
             return;
         }
         if (this._disabled === true || this._level < index) {
@@ -84,13 +84,13 @@ levels.forEach(function (level, index) {
         }
 
         let d = new Date();
-        let day = [d.getFullYear(), normalize(d.getMonth() + 1), normalize(d.getDate())].join('/');
-        let time = [normalize(d.getHours()), normalize(d.getMinutes()), normalize(d.getSeconds())].join(':');
+        let day = [d.getFullYear(), normalize(d.getMonth() + 1), normalize(d.getDate())].join("/");
+        let time = [normalize(d.getHours()), normalize(d.getMinutes()), normalize(d.getSeconds())].join(":");
         let str = this._format.call(null, this.tag, day, time, level, text);
 
         //Build the message for this level and emit the data event
-        //this.push(message(level, this.tag, text), 'utf8');
-        this.emit('data', str + '\n');
+        //this.push(message(level, this.tag, text), "utf8");
+        this.emit("data", str + "\n");
         return this;
     };
 });
@@ -103,7 +103,7 @@ logty.prototype._read = function () {
 //Close the writable stream
 logty.prototype.end = function () {
     this._disabled = true;
-    this.emit('end');
+    this.emit("end");
     return this;
 };
 
