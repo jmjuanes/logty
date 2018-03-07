69419624bde2cbe53c8f8036f56c547bfd644c39
jmjuanes <josemijuanes@gmail.com>
2018-03-07 15:30:31 +0100

    Renamed levels --> labels

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let util = require("util");
 
 let timestamp = require("./lib/timestamp.js");
 
-//Default levels list
-let levels = ["fatal", "error", "warning", "notice", "info", "debug"];
+//Default labels list
+let labels = ["fatal", "error", "warning", "notice", "info", "debug"];
 
 //Logty readable stream
 let logty = function (opt) {
@@ -22,18 +22,18 @@ let logty = function (opt) {
     stream.Readable.call(this, {encoding: opt.encoding});
 
     this._tag = (typeof opt.tag === "string") ? opt.tag.trim() : null;
-    this._level = levels.length;
+    this._level = labels.length;
     this._disabled = false;
 
     //Default message format
-    this._format = function (tag, level, text) {
+    this._format = function (tag, label, text) {
         let list = [];
         if (tag) {
             //Add the tag if it is provided
             list.push("[" + tag + "]");
         }
         list.push("[" + timestamp("YYYY:MM:DD hh:mm:ss") + "]");
-        list.push("[" + level.toUpperCase() + "]");
+        list.push("[" + label.toUpperCase() + "]");
         list.push(text.trim());
         return list.join(" ");
     };
@@ -46,12 +46,12 @@ util.inherits(logty, stream.Readable);
 
 //Set the minimum level
 logty.prototype.setLevel = function (level) {
-    let index = levels.indexOf(level);
+    let index = labels.indexOf(level);
     if (typeof level === "string" && index > -1) {
         this._level = index;
     }
     else {
-        this._level = levels.length;
+        this._level = labels.length;
     }
     return this;
 };
@@ -65,9 +65,9 @@ logty.prototype.setFormat = function (format) {
     return this;
 };
 
-//Register a method to emit a log message for each level
-levels.forEach(function (level, index) {
-    logty.prototype[level] = function (text) {
+//Register a method to emit a log message for each label
+labels.forEach(function (label, index) {
+    logty.prototype[label] = function (text) {
         if (typeof text !== "string") {
             return;
         }
@@ -75,7 +75,7 @@ levels.forEach(function (level, index) {
             return;
         }
         //Build the message for this level and emit the data event
-        let str = this._format.call(null, this._tag, level, text);
+        let str = this._format.call(null, this._tag, label, text);
         //this.push(message(level, this._tag, text), "utf8");
         this.emit("data", str + "\n");
         return this;
@@ -97,6 +97,6 @@ logty.prototype.end = function () {
 //Export logty class
 module.exports = logty;
 
-//Let people access to levels array and timestamp script
-module.exports.levels = levels;
+//Let people access to labels array and timestamp script
+module.exports.labels = labels;
 module.exports.timestamp = timestamp;
